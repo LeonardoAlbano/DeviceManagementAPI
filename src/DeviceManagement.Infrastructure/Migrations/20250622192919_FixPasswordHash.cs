6b951928739984d3af14fca29f993bf7a2f59020
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-06-22 16:41:03 -0300

    fix: correct bud user ADM, passwordHash and seedDB

---
+++ src/DeviceManagement.Infrastructure/Migrations/20250622192919_FixPasswordHash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPasswordHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "PasswordHash",
+                value: "4AaJ9AbWA1GYMhUSgRFxX6Npy7ig1OEcFvsRcy8YBkk=");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "PasswordHash",
+                value: "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=");
+        }
+    }
+}
